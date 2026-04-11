401320234f009ecafc233ee76a6c2a65d4770378
satyaki95 <www.satyaki95@gmail.com>
2026-04-11 22:03:09 +0530

    home page

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,9 +1,15 @@
 import "./index.scss";
+
 export const HomeScreen = () => {
   return (
-    <div className="root-container">
+    <div className="home-container">
       <div className="left-container">
-        <h1>Left Conatainer</h1>
+        <div className="items-container">
+          <img src="logo.png" />
+          <h1>Code Desk</h1>
+          <h2>Code.Compile.Debug</h2>
+          <button>Create New Playground</button>
+        </div>
       </div>
       <div className="right-container">
         <h1>right container</h1>
