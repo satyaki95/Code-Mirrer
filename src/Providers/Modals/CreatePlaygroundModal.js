d99da42693fee834fd2b4999874b3ebeeb1fd1c5
satyaki95 <www.satyaki95@gmail.com>
2026-04-15 21:37:04 +0530

    style create modal

--- src/Providers/Modals/CreatePlaygroundModal.js
+++ src/Providers/Modals/CreatePlaygroundModal.js
@@ -1,5 +1,29 @@
 import "./createPlaygroundModal.scss";
 
 export const CreatePlaygroundModal = () => {
-  return <div>CreatePlaygroundModal</div>;
+  return (
+    <div className="modal-container">
+      <form className="modal-body">
+        <span className="material-icons close">close</span>
+        <h1>Create New Playground</h1>
+        <div className="item">
+          <p>Enter Folder Name</p>
+          <input />
+        </div>
+        <div className="item">
+          <p>Enter Card Name</p>
+          <input />
+        </div>
+        <div className="item">
+          <select>
+            <option value="cpp">CPP</option>
+            <option value="java">Java</option>
+            <option value="javascript">JavaScript</option>
+            <option value="python">Python</option>
+          </select>
+          <button type="submit">Create Playground</button>
+        </div>
+      </form>
+    </div>
+  );
 };
