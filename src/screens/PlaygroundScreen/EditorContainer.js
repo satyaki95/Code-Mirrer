56d34813d672ffca494c3633a57fc149190c5d14
satyaki95 <www.satyaki95@gmail.com>
2026-04-28 12:56:46 +0530

    import and export

--- src/screens/PlaygroundScreen/EditorContainer.js
+++ src/screens/PlaygroundScreen/EditorContainer.js
@@ -1,3 +1,4 @@
+import { useRef, useState } from "react";
 import "./editorContainer.scss";
 import Editor from "@monaco-editor/react";
 
@@ -6,10 +7,65 @@ const editorOption = {
   wordWraap: "on",
 };
 
+const fileExtensionMapping = {
+  cpp: "cpp",
+  javascript: "js",
+  python: "py",
+  java: "java",
+};
+
 export const EditorContainer = () => {
+  const [code, setCode] = useState("");
+  const [language, setLanguage] = useState("cpp");
+  const [theme, setTheme] = useState("vs-dark");
+  const codeRef = useRef();
+
   const onChangeCode = (newCode) => {
-    
+    codeRef.current = newCode;
   };
+
+  const importCode = (event) => {
+    const file = event.target.files[0];
+    const fileType = file.type.includes("text");
+    if (fileType) {
+      const fileReader = new FileReader();
+      fileReader.readAsText(file);
+      fileReader.onload = function (value) {
+        const importedCode = value.target.result;
+        setCode(importedCode);
+        codeRef.current = importedCode;
+      };
+    } else {
+      alert("Please Choose a Program File");
+    }
+  };
+
+  const exportCode = () => {
+    const codeValue = codeRef.current?.trim();
+    if (!codeValue) {
+      alert("Please type some code in the editor before exporting");
+    }
+    //Create a blob / instant file in the memory
+    else {
+      const codeBlob = new Blob([codeValue], { type: "text/plain" });
+      const downloadUrl = URL.createObjectURL(codeBlob);
+
+      const link = document.createElement("a");
+      link.href = downloadUrl;
+
+      link.download = `code.${fileExtensionMapping[language]}`;
+      link.click();
+    }
+  };
+
+  const onChangeLanguage = (e) => {
+    setLanguage(e.target.value);
+  };
+
+  const onChangeTheme = (e) => {
+    setTheme(e.target.value);
+  };
+
   return (
     <div className="root-editor-container">
       <div className="editor-header">
@@ -19,14 +75,14 @@ export const EditorContainer = () => {
           <button>Save Code</button>
         </div>
         <div className="editor-right-container">
-          <select>
+          <select onChange={onChangeLanguage} value={language}>
             <option value="cpp">cpp</option>
             <option value="java">java</option>
             <option value="javascript">javascript</option>
             <option value="python">python</option>
           </select>
 
-          <select>
+          <select onChange={onChangeTheme} value={theme}>
             <option value="vs-dark">Vs-Dark</option>
             <option value="vs-light">Vs-Light</option>
           </select>
@@ -35,9 +91,10 @@ export const EditorContainer = () => {
       <div className="editor-body">
         <Editor
           height={"100%"}
-          language={"javascript"}
+          language={language}
           options={editorOption}
-          theme={"vs-dark"}
+          theme={theme}
+          value={code}
           onChange={onChangeCode}
         />
       </div>
@@ -50,8 +107,8 @@ export const EditorContainer = () => {
           <span className="material-icons">cloud_upload</span>
           <span>Import Code</span>
         </label>
-        <input type="file" id="import-code" />
-        <button className="btn">
+        <input type="file" id="import-code" onChange={importCode} />
+        <button className="btn" onClick={exportCode}>
           <span className="material-icons">cloud_download</span>
           <span>Export Code</span>
         </button>
--- src/screens/PlaygroundScreen/index.js
+++ src/screens/PlaygroundScreen/index.js
@@ -1,10 +1,42 @@
 import { useParams } from "react-router-dom";
 import "./index.scss";
 import { EditorContainer } from "./EditorContainer";
+import { useState } from "react";
 
 export const PlaygroundScreen = () => {
   const params = useParams();
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
   const { fileId, folderId } = params;
+
+  const importInput = (e) => {
+    const file = e.target.files[0];
+    const fileType = file.type.includes("text");
+    if (fileType) {
+      const fileReader = new FileReader();
+      fileReader.readAsText(file);
+      fileReader.onload = (e) => {
+        setInput(e.target.result);
+      };
+    } else {
+      alert("Please Choose a Program File");
+    }
+  };
+
+  const exportOutput = () => {
+    // download a text file the contentss for output text area
+    const outputValue = output.trim();
+    if (!outputValue) {
+      alert("Output is Empty");
+    } else {
+      const blob = new Blob([outputValue], { type: "text/plain" });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = `output.txt`;
+      link.click();
+    }
+  };
 
   return (
     <div className="playground-container">
@@ -22,19 +54,26 @@ export const PlaygroundScreen = () => {
               <span className="material-icons">cloud_upload</span>
               <b className="">Import Input</b>
             </label>
-            <input type="file" id="input" />
+            <input type="file" id="input" onChange={importInput} />
           </div>
-          <textarea></textarea>
+          <textarea
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          ></textarea>
         </div>
         <div className="input-output-container">
           <div className="input-header">
             <b>Output:</b>
-            <button className="icon-container">
+            <button className="icon-container" onClick={exportOutput}>
               <span className="material-icons">cloud_download</span>
               <b>Export Output</b>
             </button>
           </div>
-          <textarea readOnly></textarea>
+          <textarea
+            readOnly
+            value={output}
+            onChange={(e) => setOutput(e.target.value)}
+          ></textarea>
         </div>
       </div>
     </div>
